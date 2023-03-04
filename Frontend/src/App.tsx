323e48dc909430b2f79c7df6a28be9b3579173ed
Arsoty <lunar1645@gmail.com>
2023-03-04 14:19:41 +0200

    main screen in progress

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import background from "./assets/background.jpg";
 import Header from "./components/Header";
+import styles from "./styles/MainStyles";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ ...styles.main }}>
       <Header />
-      <text>Voluncare</text>
     </div>
   );
 }
