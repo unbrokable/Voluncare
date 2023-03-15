2550e0affa96b1b7b68ea977759ee373a5e8037e
unbrokable <80967885+unbrokable@users.noreply.github.com>
2023-03-15 19:15:51 +0200

    Test pushing ui

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,10 +2,13 @@ import "./App.css";
 import Header from "./components/Header";
 
 function App() {
+
+  const { VITE_API_URL_API } = import.meta.env; 
+
   return (
     <div className="App">
       <Header />
-      <text>Voluncare</text>
+      <div>Test variable {VITE_API_URL_API}</div>
     </div>
   );
 }
